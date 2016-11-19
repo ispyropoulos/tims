cb24aaaf4c24f137d98893054212d5ca600ea897
Ilias Spyropoulos <ilias@heavymelon.com>
2016-11-19 20:08:18 +0200

    Cache the TIMS feed to vastly increase endpoint's roundtrip times
    The TIMS feed refreshes its data every 5 minutes. This allows for
    a similar cache window. At worst, the local endpoint will have
    stale data for a total time of 9 minutes and 59 seconds.

--- app/controllers/api/v1/disruptions_controller.rb
+++ app/controllers/api/v1/disruptions_controller.rb
@@ -2,18 +2,27 @@ module Api
   module V1
     class DisruptionsController < ApiController
       def index
-        # Download XML directly on disk
-        IO.copy_stream(open(TIMS_FEED_URL), 'tmp/tims_feed.xml')
+        render json: disruption_points_array.to_json
+      end
 
-        # We use Nokogiri's SAX parser (event-drive) to avoid parsing huge DOM
-        # representations of the XML feed. This would potentially kill our
-        # server memory and it'd be considerably slower.
-        # We also use an XML file on the disk as the source, instead of streaming
-        # it directly from the internet to keep memory utilisation at a minimum.
-        tims_document = TimsDocument.new
-        Nokogiri::XML::SAX::Parser.new(tims_document).parse_file('tmp/tims_feed.xml')
+      private
 
-        render json: tims_document.points.to_json
+      def disruption_points_array
+        Rails.cache.fetch('disruption_points', expires_in: 5.minutes, race_condition_ttl: 10) do
+          # Download XML directly on disk
+          IO.copy_stream(open(TIMS_FEED_URL), 'tmp/tims_feed.xml')
+
+          # We use Nokogiri's SAX parser (event-driven) to avoid parsing huge
+          # DOM representations of the XML feed. This could potentially exhaust
+          # our server memory and also be considerably slower.
+          # Additionally, we use an XML file from the disk as the source,
+          # instead of streaming it directly from the internet to keep memory
+          # utilisation as low as possible.
+          tims_document = TimsDocument.new
+          Nokogiri::XML::SAX::Parser.new(tims_document).parse_file('tmp/tims_feed.xml')
+
+          tims_document.points
+        end
       end
     end
   end
